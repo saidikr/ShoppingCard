aba3258f4280f09aee8b28450d695a02a86a1286
said ikrouberkane <said.ikrouberkane@gmail.com>
2022-08-26 16:55:00 +0100

    finished

--- js/js.js
+++ js/js.js
@@ -2,18 +2,22 @@
 //card variables
 
 
-var tshirtprice=250;
-var totalpricenum=0;//total price of both elements
-var totalpricenum1=250;//totalprice of the tshirt
-var totalpricenum2=250;//totalprice of the hoodie
+
+
 var quantitynum1=1;//quantity of tshirt
 var quantitynum2=1;//quantity of hoodie
 var card=document.querySelectorAll(".card");//nodelist of two elements 
 var tshirtHeart=false;
 var hoodieHeart=false;
 var totalprice=document.querySelector(".total-price");//total price text
-var hoodieprice=card[0].childNodes[3].childNodes[1].childNodes[1].textContent;
-console.log(hoodieprice)
+var tshirtprice=parseInt(card[0].childNodes[3].childNodes[1].childNodes[1].textContent);
+var hoodieprice=parseInt(card[1].childNodes[3].childNodes[1].childNodes[1].textContent);
+var tp=tshirtprice;
+var hp=hoodieprice;
+var totalpricenum1=tshirtprice;//totalprice of the tshirt
+var totalpricenum2=hoodieprice;//totalprice of the hoodie
+var totalpricenum=totalpricenum1+totalpricenum2;//total price of both elements
+totalprice.textContent=totalprice.textContent="Total Price : "+totalpricenum+" Da";
 
 
 
@@ -22,20 +26,22 @@ console.log(hoodieprice)
 
 //--------------------------------------Tshirt---------------------------------
 
-var tshirt=card[0].childNodes[3];//tshirt price-text
+var tshirt=card[0].childNodes[3].childNodes[1].childNodes[1];//tshirt price-text
+console.log(tshirt);
 var quantity1=card[0].childNodes[5].childNodes[1];//tshirt quantity-text
 
 
 //plus
 
 var plus1=card[0].childNodes[7].childNodes[1];//tshirt plus
+console.log(plus1);
 
 plus1.addEventListener("click",function(ev){
-    tshirtprice+=250;
-    totalpricenum1+=250;
+    tp+=tshirtprice;
+    totalpricenum1+=tshirtprice;
     totalpricenum=totalpricenum1+totalpricenum2;
     quantitynum1+=1;
-    tshirt.textContent="Price : "+tshirtprice+" Da";
+    tshirt.textContent=tp;
     totalprice.textContent="Total Price : "+totalpricenum+" Da";
     quantity1.textContent="Quantity : "+quantitynum1;
 })
@@ -47,19 +53,25 @@ plus1.addEventListener("click",function(ev){
 var minus1=card[0].childNodes[7].childNodes[3];//tshirt minus
 
 minus1.addEventListener("click",function(ev){
-    if(tshirtprice>0){
-        tshirtprice-=250;
+    if(tp>0){
+        tp-=tshirtprice;
     }
     if(totalpricenum1>0){
-        totalpricenum1-=250;
+        totalpricenum1-=tshirtprice;
         totalpricenum=totalpricenum1+totalpricenum2;
     }
     if(quantitynum1>0){
         quantitynum1-=1;
     }
-    tshirt.textContent="Price : "+tshirtprice+" Da";
+    tshirt.textContent=tp;
     totalprice.textContent="Total Price : "+totalpricenum+" Da";
     quantity1.textContent="Quantity : "+quantitynum1;
+    if(quantitynum1===0){
+        card[0].remove();
+     totalpricenum1=0;
+     totalpricenum=totalpricenum1+totalpricenum2;
+     totalprice.textContent="Total Price : "+totalpricenum+" Da";
+    }
 })
 
 //trash
@@ -100,9 +112,7 @@ heart1.addEventListener("click",function(ev){
 
 
 
-
-
-var hoodie=card[1].childNodes[3];//hoodie price-text
+var hoodie=card[1].childNodes[3].childNodes[1].childNodes[1];//hoodie price-text
 var quantity2=card[1].childNodes[5].childNodes[1];//hoodie quantity-text
 
 
@@ -112,11 +122,11 @@ var quantity2=card[1].childNodes[5].childNodes[1];//hoodie quantity-text
 var plus2=card[1].childNodes[7].childNodes[1];//hoodie plus
 
 plus2.addEventListener("click",function(ev){
-    hoodieprice+=250;
-    totalpricenum2+=250;
+    hp+=hoodieprice;
+    totalpricenum2+=hoodieprice;
     totalpricenum=totalpricenum1+totalpricenum2;
     quantitynum2+=1;
-    hoodie.textContent="Price : "+hoodieprice+" Da";
+    hoodie.textContent=hp;
     totalprice.textContent="Total Price : "+totalpricenum+" Da";
     quantity2.textContent="Quantity : "+quantitynum2;
 })
@@ -130,19 +140,26 @@ plus2.addEventListener("click",function(ev){
 var minus1=card[1].childNodes[7].childNodes[3];//hoodie minus
 
 minus1.addEventListener("click",function(ev){
-    if(hoodieprice>0){
-        hoodieprice-=250;
+    if(hp>0){
+        hp-=hoodieprice;
     }
     if(totalpricenum2>0){
-        totalpricenum2-=250;
+        totalpricenum2-=hoodieprice;
         totalpricenum=totalpricenum1+totalpricenum2;
     }
     if(quantitynum2>0){
         quantitynum2-=1;
     }
-    hoodie.textContent="Price : "+hoodieprice+" Da"
+    hoodie.textContent=hp
     totalprice.textContent="Total Price : "+totalpricenum+" Da"
     quantity2.textContent="Quantity : "+quantitynum2
+    if(quantitynum2===0){
+        card[1].remove();
+     totalpricenum2=0;
+     totalpricenum=totalpricenum1+totalpricenum2;
+     totalprice.textContent="Total Price : "+totalpricenum+" Da";
+    }
+
 })
 
 
